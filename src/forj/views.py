3c20bd8a1f0daa1d24a991ef0995258451e94860
hallowslab <hallow.tech.lab@gmail.com>
2026-01-01 23:56:23 +0000

    forj index

--- src/forj/views.py
+++ src/forj/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
-    return HttpResponse("Hello from FÖᚱJ App!")
+    return render(request, "forj_index.html")
