293e199710d15013ee6bcf573efd6830480c1082
hallowslab <hallow.tech.lab@gmail.com>
2026-01-04 16:03:28 +0000

    arka dashboard(WIP)

--- src/arka/views.py
+++ src/arka/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .dashboard.core import check_database, check_redis, check_rabbitmq
 
 @login_required
 def dashboard(request):
-    return render(request, "dashboard.html")
+    context = {
+        "checks": {
+            "database": check_database(),
+            "redis": check_redis(),
+            "rabbitmq": check_rabbitmq(),
+        }
+    }
+    return render(request, "dashboard.html", context)
+
 
 @login_required
 def profile(request):
